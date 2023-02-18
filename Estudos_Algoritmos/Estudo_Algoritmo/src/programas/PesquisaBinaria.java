7621aa382f03da3d54292687e56c57775f94f6ff
Gustavo S. <gzttvv@gmail.com>
2023-02-18 19:55:54 -0300

    tentando criar a base

--- Estudos_Algoritmos/Estudo_Algoritmo/src/programas/PesquisaBinaria.java
+++ Estudos_Algoritmos/Estudo_Algoritmo/src/programas/PesquisaBinaria.java
@@ -4,9 +4,24 @@ public class PesquisaBinaria {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 int baixo, alto,meio,chute,item,lista;
+		int [] minhalista = new int[10];
 		
+		baixo = 0 ;
+		alto = minhalista.length-1;
+		meio = (baixo+alto) / 2 ;
+		chute = minhalista[meio];
+		
+		item = 7;
+		
+		if (chute < item) {
+			baixo = meio +1;
+		} 
 		
 
 	}
+	
+	
+	
 
 }
