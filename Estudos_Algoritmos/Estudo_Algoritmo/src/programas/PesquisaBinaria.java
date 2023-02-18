0e050c52e7e2fe131df86b73ddb0baca22546977
Gustavo S. <gzttvv@gmail.com>
2023-02-18 10:21:38 -0300

    Arquivos criados

---
+++ Estudos_Algoritmos/Estudo_Algoritmo/src/programas/PesquisaBinaria.java
@@ -0,0 +1,12 @@
+package programas;
+
+public class PesquisaBinaria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Teste");
+
+	}
+
+}
